28589923fadc67b9f9967e9b4f69dc201d459da3
pielichl <lukaszpielichowski@icloud.com>
2022-04-18 12:52:13 +0200

    Add var computerMove & playermove to script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var computerMove, playerMove;
+computerMove = 'kamień';
+playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
