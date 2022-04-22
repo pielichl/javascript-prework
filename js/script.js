cabede643a69c8607e593e4a32c4afd8cc29645f
pielichl <lukaszpielichowski@icloud.com>
2022-04-22 23:11:15 +0200

    Adjust var declaration to const

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
  */
 
     function buttonClicked(argButtonName) {
-    clearMessages();
-    console.log(argButtonName + ' został kliknięty');
+      clearMessages();
+      console.log(argButtonName + ' został kliknięty');
     
     
     function getMoveName(argMoveId) {
@@ -40,11 +40,11 @@
       printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
     } 
   
-    playerMove = argButtonName;
+    const playerMove = argButtonName;
     console.log('ruch gracza to: ' + playerMove);
-    randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('wylosowana liczba to: ' + randomNumber);
-    computerMove = getMoveName(randomNumber);
+    const computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
     displayResult(playerMove, computerMove);
     }
